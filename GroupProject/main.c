3a3a51d3fe3394a1b76dc2430d2e5ca228321009
Yue-Jia <52579897+Yue-Jia@users.noreply.github.com>
2019-07-24 14:08:41 -0400

    Add files via upload

---
+++ GroupProject/main.c
@@ -0,0 +1,44 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+typedef struct runner{
+    char* name;
+    char gender;
+    char* country;    
+    int bib;
+    double record;
+}runnerType,*runner;
+
+int main() {
+
+    return 0;
+}
+runner readFile(){
+
+}
+void print(runner){
+
+}
+void add(){
+
+}
+void detail(){
+
+}
+void edit(){
+
+}
+void delete(){
+
+}
+int search(){
+
+}
+runner sort(){
+
+}
