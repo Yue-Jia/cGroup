20d0914dbabb4d0bc8db7e4e2afbd8539607e256
Administrator <Administrator@DESKTOP-5HT97GC>
2019-07-30 23:35:55 -0400

    read bib,name in records from file

--- GroupProject/main.c
+++ GroupProject/main.c
@@ -7,9 +7,6 @@
 //remove new line '\n' and return '\r' characters
 #define REMOVERN(str) str[strcspn(str,"\r\n")] = 0;
 
-// The maximum number of quotes we can read from input file
-#define MAX_QUOTES 10050
-
 // The maximum size of the string in the file.
 #define MAX_LEN 1000
 
@@ -19,64 +16,77 @@ typedef struct runner{
     char country[3];
     int bib;
     int record;
+    struct runner* next;
 }runnerType,*runner;
 
-void readFile(char**, size_t*);
-void print(char**, size_t);
+void readFile(runner*, int*);
+void print(runner);
 
 int main() {
-    // Create array of pointers. Each pointer should point to heap memory where
-    // each quote is kept. I.e. arr[0] points to quote N1 saved on the heap.
-    char *arr[MAX_QUOTES];
+    // Create head pointers. This pointer should point to the first node of the linked list
+    // The linked list should be saved on the heap.
+    runner head = NULL;
 
     // Number of quotes in the file quotes.txt. We assume initially that file has MAX_QUOTES quotes.
     // Must be adjusted in the function read_in when the file is read!
-    size_t size=MAX_QUOTES; 
+    int size=0; 
 
     // Read quotes from file and place them into array arr. 
     // Note: size reflects how many quotes we read from the file (between 0 and MAX_QUOTES).
-    readFile(arr, &size);
+    readFile(&head, &size);
 
     //selection_sort(arr, size);
     
-    print(arr, size);
+    //print(head);
     //write_out(arr, size);
     
     //free_memory(arr, size);
     return 0;
 }
-void readFile(char** arr, size_t* quotes_count){
+void readFile(runner* head, int* count){
     FILE *fp;
     fp = fopen("data.txt","r");
     if(fp == NULL){
         printf("Error reading file");
         exit(1);
     }
-    int i = 0;
-    while(!feof(fp) && i+1 <= MAX_QUOTES){
+    while(!feof(fp)){
         char temp[MAX_LEN];
         if(fgets(temp,sizeof(temp),fp)){
             REMOVERN(temp);
             if (temp[0]){
-                arr[i] = (char*) calloc(strlen(temp)+1,sizeof(char));
-                if(arr[i] == NULL){
-                    printf("Cannot allocate memory for quotes No.%d", i);
-                    exit(1);
+                runner node = (runner) malloc(sizeof(runnerType));
+                if(node == NULL){
+                    printf("Cannot allocate memory for more runner");
+                    return;
                 }
-                strncpy(arr[i],temp,strlen(temp));
-                i++;
+                char* token;
+                //bib
+                token = strtok (temp,"\t");
+                node->bib = (int) strtol(token,NULL,10);
+                //name
+                token = strtok (NULL, "\t");
+                node->name = (char*) calloc(strlen(token)+1,sizeof(char));
+                strncpy(node->name, token, strlen(token));
+                node->next = NULL;
+                *count = *count + 1;
+                if(*count == 1){
+                    *head = node;
+                }else{
+                    node->next = *head;
+                    *head = node;
+                }
             }
         }
     }
-    if(i==MAX_QUOTES){
-        printf("Warning: Number of quotes in the file exceeds MAX_QUOTES allowed by this program. Partial of them will be processed.\n");
-    }
-    *quotes_count = i;
+    printf("%d records loaded.",*count);
     fclose(fp);
 }
-void print(char** arr, size_t quotes_count){
-    for(int i = 0;i < quotes_count; i++){
-        printf("%s\n",arr[i]);
+void print(runner head){
+    runner current = head;
+    while(current!=NULL){
+        printf("%6d%30s\n",current->bib,current->name);
+        current=current->next;
     }
 }
 void add(){
